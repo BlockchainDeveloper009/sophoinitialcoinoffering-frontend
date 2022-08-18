46907bbcae833dc0440c07bc1045c08888c28624
alpha@DESKTOP-6F6AEHR <blkcdev@gmail.com>
2022-08-18 14:22:01 -0500

    fixed network id to match Polygon chain id 80001

--- pages/index.js
+++ pages/index.js
@@ -262,9 +262,9 @@ export default function Home() {
 
     // If user is not connected to the Rinkeby network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
-    if (chainId !== 4) {
-      window.alert("Change the network to Rinkeby");
-      throw new Error("Change network to Rinkeby");
+    if (chainId !== 80001) {
+      window.alert("Change the network to Polygon Mumbai");
+      throw new Error("Err: Change network to Polygon Mumbai testnet");
     }
 
     if (needSigner) {
